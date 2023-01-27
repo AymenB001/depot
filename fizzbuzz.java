e9d9f1a7167d25ffb69d424cab02a6f57c98146a
Aymen Benyahia <aymenbeny7@gmail.com>
2023-01-27 15:01:03 -0500

    fizzbuzz.java

---
+++ fizzbuzz.java
@@ -0,0 +1,19 @@
+import java.util.Scanner
+
+public class T{
+
+
+public static void main(String args[]){
+   for(int i = 1; i <= 100; i++){
+        if(i % 3 == 0 && i % 5 == 0){
+           System.out.println("\n%d: fizz buzz",i);
+        }else if (i % 5 == 0){
+         System.out.println("\%d:buzz",i); 
+        }else if (i%3 == 0){
+        System.out.println("\n%d: fizz",i);
+        }
+   }
+   System.out.printf("\n");
+ }
+}
+
